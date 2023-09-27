ce3315b906bbfb28e262f80f852cc78cd14727fe
claratheisen <kiwib1r9@gmail.com>
2023-09-27 15:35:04 -0300

    atualização 1 do arquivo main
    implementação da biblioteca push e zoom in com janela virtual

--- main.lua
+++ main.lua
@@ -1,10 +1,21 @@
--- definição da altura e largura da janela do jogo
+--[[ bibliotecas ]]
+push = require 'push'
 
+
+--[[ definição da altura e largura da janela do jogo]]
 LARGURA_JANELA = 1280
 ALTURA_JANELA = 720
 
+LARGURA_VIRTUAL = 432
+ALTURA_VIRTUAL = 243
+
+
+--[[ inicialização]]
 function love.load()
-    love.window.setMode(LARGURA_JANELA, ALTURA_JANELA, {
+    -- def de filtro nearest como padrão para evitar o aspecto 'blur' do zoom
+    love.graphics.setDefaultFilter('nearest', 'nearest')    
+    -- setup da janela com utilização da biblioteca push
+    push:setupScreen(LARGURA_VIRTUAL, ALTURA_VIRTUAL, LARGURA_JANELA, ALTURA_JANELA, {
         fullscreen = false,
         resizable = false,
         vsync = true -- sincronizado com a taxa de refresh do monitor
@@ -12,17 +23,32 @@ function love.load()
 )
 end
 
+
+--[[ definição de fechamento de janela ao pressionar a tecla escape]]
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+
 --[[
 function love.update(dt)
 end]]
 
+
+--[[ renderização]]
 function love.draw()
+    push:apply('start')         -- aplica o zoom
+    
     love.graphics.printf(
         'Hello Pong!',          -- texto
         0,                      -- x inicial
-        ALTURA_JANELA/2 - 6,    -- y inicial (subtraindo 6 pois a fonte padrão tem altura 12)
-        LARGURA_JANELA,         -- parametro de alinhamento
+        ALTURA_VIRTUAL/2 - 6,    -- y inicial (subtraindo 6 pois a fonte padrão tem altura 12)
+        LARGURA_VIRTUAL,         -- parametro de alinhamento
         'center')               -- tipo de alinhamento
+    
+    push:apply('end')           -- fim da aplicação de zoom
 end
 
 
