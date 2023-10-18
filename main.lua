f395f3f9e9472db20bc7e74c9e9f923734112738
claratheisen <kiwib1r9@gmail.com>
2023-10-18 10:48:15 -0300

    versao 1.0 final do arquivo main
    estado de vitoria + sons

--- main.lua
+++ main.lua
@@ -24,7 +24,17 @@ function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')    
     -- setup fontes
     fonteP = love.graphics.newFont('Minecraftia-Regular.ttf', 8)
+    fonteM = love.graphics.newFont('Minecraftia-Regular.ttf', 24)
     fonteG = love.graphics.newFont('Minecraftia-Regular.ttf', 32)
+
+    -- inicialização de sons
+    sons = {
+        ['paddle_hit'] = love.audio.newSource('paddle_hit.wav', "static"),
+        ['wall_hit'] = love.audio.newSource('hit_wall.wav', "static"),
+        ['score'] = love.audio.newSource('point_score.wav', "static"),
+        ['win'] = love.audio.newSource('win.mp3', "static"),
+        ['music'] = love.audio.newSource('8-bit-universe.mp3', "static")
+    }
 
     -- inicialização das variaveis das raquetes
     ALTURA_RAQUETE = 26
@@ -41,6 +51,8 @@ function love.load()
     -- variavel que aponta o jogador que esta servindo na rodada 
     servidor = math.random(2) == 1 and 1 or 2
     
+    vencedor = 0
+
     -- inicialização das variaveis da bola
     ALTURA_BOLA = 4
     LARGURA_BOLA = 4
@@ -71,9 +83,16 @@ function love.keypressed(key)
         love.event.quit()
     elseif key == 'enter' or key == 'return' then 
         if estadoJogo == 'inicio' then
+            sons['music']:setVolume(0.3)
+            sons['music']:setLooping(true)
+            sons['music']:play()
             estadoJogo = 'servindo'
+
         elseif estadoJogo == 'servindo' then
             estadoJogo = 'jogando'
+
+        elseif estadoJogo == 'vitoria' then
+            estadoJogo = 'inicio'
         end
     end
 end
@@ -91,28 +110,44 @@ function love.update(dt)
         raqueteE:reset()
         raqueteD:reset()
 
+        pontosD = 0
+        pontosE = 0
 
-    elseif estadoJogo == 'servindo' then
-        
     elseif estadoJogo == 'jogando' then 
 
         -- atualiza placar 
         if bola.x <= 0 then
             pontosD = pontosD + 1
+            sons['score']:play()
             servidor = 1
             -- centraliza posição da bola
             bola:reset()
             bola.dx = VELOCIDADE_RAQUETE
-            estadoJogo = 'servindo'
+            if pontosD >= 2 then
+                vencedor = 2
+                estadoJogo = 'vitoria'
+                sons['music']:stop()
+                sons['win']:play()
+            else
+                estadoJogo = 'servindo'
+            end
         end
 
         if bola.x >= LARGURA_VIRTUAL - 4 then
             pontosE = pontosE + 1
+            sons['score']:play()
             sevidor = 2
             -- centraliza posição da bola
             bola:reset()
             bola.dx = -VELOCIDADE_RAQUETE
-            estadoJogo = 'servindo'
+            if pontosE >= 2 then
+                vencedor = 1
+                estadoJogo = 'vitoria'
+                sons['music']:stop()
+                sons['win']:play()
+            else
+                estadoJogo = 'servindo'
+            end
         end
         -- atualiza posição da bola 
         bola:update(dt)
@@ -134,6 +169,32 @@ function love.update(dt)
         else
             raqueteD.dy = 0
         end
+
+        -- colisão check
+        if bola:colide(raqueteE) then
+            bola.dx = -bola.dx
+            bola.x = raqueteE.x + raqueteE.largura
+            sons['paddle_hit']:play()
+            
+        end
+
+        if bola:colide(raqueteD) then
+            bola.dx = -bola.dx
+            bola.x = raqueteD.x - bola.largura 
+            sons['paddle_hit']:play()
+        end
+
+        if bola.y < 0 then
+            bola.dy = -bola.dy
+            bola.y = 0
+            sons['wall_hit']:play()
+        end
+
+        if bola.y > ALTURA_VIRTUAL then
+            bola.dy = -bola.dy
+            bola.y = ALTURA_VIRTUAL - bola.altura
+            sons['wall_hit']:play()
+        end
     end
 end
 
@@ -148,26 +209,6 @@ function love.draw()
     
     love.graphics.clear(164/255, 222/255, 191/255, 255/255)     -- background clear color
 
-    -- colisão check
-    if bola:colide(raqueteE) then
-        bola.dx = -bola.dx
-        bola.x = raqueteE.x + raqueteE.largura
-    end
-
-    if bola:colide(raqueteD) then
-        bola.dx = -bola.dx
-        bola.x = raqueteD.x - bola.largura 
-    end
-
-    if bola.y < 0 then
-        bola.dy = -bola.dy
-        bola.y = 0
-    end
-
-    if bola.y > ALTURA_VIRTUAL then
-        bola.dy = -bola.dy
-        bola.y = ALTURA_VIRTUAL - bola.altura
-    end
 
     -- bola
     bola:render()
@@ -192,12 +233,17 @@ function love.draw()
         love.graphics.printf('Press Enter to Play!', 0 , 40, LARGURA_VIRTUAL, 'center')
     elseif estadoJogo == 'servindo' then
         love.graphics.printf('Player' .. tostring(servidor) .. 's turn to Serve', 0 , 20, LARGURA_VIRTUAL, 'center')
-        love.graphics.printf('Press Enter to Serve!', 0 , 40, LARGURA_VIRTUAL, 'center')
+        love.graphics.printf('press enter to serve', 0 , 40, LARGURA_VIRTUAL, 'center')
+    elseif estadoJogo == 'vitoria' then
+        love.graphics.setFont(fonteM)
+        love.graphics.printf('Player ' .. tostring(vencedor) .. " wins!", 0, 20, LARGURA_VIRTUAL, 'center')
+        love.graphics.setFont(fonteP)
+        love.graphics.printf('press enter to restart', 0, 54, LARGURA_VIRTUAL, 'center')
     end
+
     -- placar
-    love.graphics.setFont(fonteG)
-    love.graphics.print(pontosE, LARGURA_VIRTUAL/2 - 50, ALTURA_VIRTUAL/3)
-    love.graphics.print(pontosD, LARGURA_VIRTUAL/2 + 30, ALTURA_VIRTUAL/3)
+    displayPlacar()
+
     push:apply('end')           -- fim da aplicação de zoom
 end
 
@@ -205,4 +251,11 @@ end
 function displayFPS()
     love.graphics.setFont(fonteP)
     love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 40, 20)
+end
+-- função para mostrar placar
+function displayPlacar()
+    -- placar
+    love.graphics.setFont(fonteG)
+    love.graphics.print(pontosE, LARGURA_VIRTUAL/2 - 50, ALTURA_VIRTUAL/3)
+    love.graphics.print(pontosD, LARGURA_VIRTUAL/2 + 30, ALTURA_VIRTUAL/3)
 end
